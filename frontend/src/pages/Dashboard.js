23e2486a8e1afabab270d28a1de1cd023bd39d58
Srimitha <srimitha@gmail.com>
2026-05-11 10:38:26 +0530

    fixed vercel eslint errors

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../services/api";
